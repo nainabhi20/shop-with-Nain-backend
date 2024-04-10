808e02abba8b901c1b2c9eba2813a5fead3b9038
nainabhi20 <abhisheknain1200@gmail.com>
2024-04-10 15:40:19 +0530

    change cors setting

--- src/main/java/com/abhishek/config/AppConfig.java
+++ src/main/java/com/abhishek/config/AppConfig.java
@@ -40,10 +40,7 @@ public class AppConfig {
 						cfg.setAllowedOrigins(Arrays.asList(
 
 								"http://localhost:3000",
-								"http://localhost:4000",
-								"http://localhost:4200",
-								"https://shopwithzosh.vercel.app",
-								"https://ecommerce-angular-blue.vercel.app/"
+								"https://shop-with-nain-frontend.onrender.com"
 
 				));
 						// cfg.setAllowedMethods(Arrays.asList("GET", "POST","DELETE","PUT"));
